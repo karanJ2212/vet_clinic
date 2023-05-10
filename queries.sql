6a6530df4491cc7ac1aa06c39658527a8c866cab
karan jain <karanjain2212@gmail.com>
2023-05-10 17:43:24 +0530

    queries

--- queries.sql
+++ queries.sql
@@ -9,3 +9,71 @@ SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT * FROM animals; -- Verify the change
+
+ROLLBACK;
+
+SELECT * FROM animals; -- Verify the rollback
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+SELECT * FROM animals; -- Verify the changes
+
+COMMIT;
+
+SELECT * FROM animals; -- Verify changes persist after commit
+
+BEGIN;
+
+DELETE FROM animals;
+
+SELECT * FROM animals; -- Verify the deletion (should return no records)
+
+ROLLBACK;
+
+SELECT * FROM animals; -- Verify records still exist after the rollback
+
+BEGIN;
+
+-- Delete animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weights to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Update negative weights to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+-- Commit the transaction
+COMMIT;
+
+vet_clinic=# SELECT COUNT(*) FROM animals;
+vet_clinic=# SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+vet_clinic=# SELECT AVG(weight_kg) FROM animals;
+SELECT neutered ,SUM(escape_attempts) AS total_escape_attempts from animals GROUP BY neutered ORDER BY total_escape_attempts DESC;
+select species, max(weight_kg) as max_weight ,min(weight_kg) as min_weight from animals group by species;
+
+SELECT species, AVG(escape_attempts) AS average_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
